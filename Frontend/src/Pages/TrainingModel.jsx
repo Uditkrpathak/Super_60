b495b803940a7973e2661ef6eb989b09deb6b315
Aryankr5252 <aryanak5252@gmail.com>
2025-06-24 03:24:55 +0530

    Roadmap

--- Frontend/src/Pages/TrainingModel.jsx
+++ Frontend/src/Pages/TrainingModel.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import HeroSection from '../Components/hero/HeroSection'
 import TrainingSchedule from '../Components/Training/TrainingSchedule'
+import Roadmap from '../Components/Training/Roadmap'
 
 const TrainingModel = () => {
   return (
@@ -8,6 +9,8 @@ const TrainingModel = () => {
       <HeroSection heading1={'More Than a Community'} heading2={'The Super 60'} subHeading={'An elite circle of creators, coders, and changemakers shaping the future together.'} badge={'CheckEvents Now'} />
     
       <TrainingSchedule />
+
+      <Roadmap />
     </div>
   )
 }
