4af7be36a4bdfdb95b7c0d61d49fd82245c941a9
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-06-24 12:21:43 -0500

    merge

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+
+import Batch from '../Components/About/Batch.jsx' 
+import WorkingModel from '../Components/About/Workingmodel.jsx'
 import HeroSection from "../Components/hero/HeroSection"
-
-
 const About = () => {
-
-  <div>
+  return (
+    <div>
 
    
      <HeroSection heading1={'Where Excellence Begins'} heading2={'Super60'} subHeading={'Empowering Future Leaders Through Passion, Purpose, and Excellence'}  />
- 
+ <Batch/>
+ <WorkingModel/>
   </div>
+  )
 }
 
 export default About
