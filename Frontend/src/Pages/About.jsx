92cbbdcd99a4877ba00e43f2bc89f6c27d629cf9
singh-jii-07 <nileshkumarsingh060@gmail.com>
2025-06-23 21:22:12 -0500

    About Hero

--- Frontend/src/Pages/About.jsx
+++ Frontend/src/Pages/About.jsx
@@ -4,7 +4,7 @@ import HeroSection from '../Components/hero/HeroSection'
 const About = () => {
   return (
     <div>
-      <HeroSection heading1={'More Than a Community'} heading2={'The Super 60'} subHeading={'An elite circle of creators, coders, and changemakers shaping the future together.'} badge={'CheckEvents Now'} />
+      <HeroSection heading1={' Where Excellence Begins'} heading2={' Super60'} subHeading={'An elite circle of creators, coders, and changemakers shaping the future together.'} badge={'CheckEvents Now'} />
     </div>
   )
 }
