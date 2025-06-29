2c3acbce2c2fbc120bc17b6e7af13ced7955c898
Ishant-179 <rajishant128@gmail.com>
2025-06-29 11:09:56 +0530

    updated blog schema

--- Backend/models/Blog.js
+++ Backend/models/Blog.js
@@ -1,49 +1,63 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    description: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    image: {
-      type: String,
-      trim: true,
-    },
-    category: {
-      type: String,
-      trim: true,
-    },
-    tags: [
-      {
-        type: String,
-        trim: true,
-      },
-    ],
-    sections: [
-      {
-        heading: {
-          type: String,
-          trim: true,
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
         },
-        content: {
-          type: String,
+        description: {
+            type: String,
+            required: true,
+            trim: true,
         },
-      },
-    ],
+        image: {
+            type: String,
+            trim: true,
+        },
+        category: {
+            type: String,
+            trim: true,
+        },
+        tags: [
+            {
+                type: String,
+                trim: true,
+            },
+        ],
+        sections: [
+            {
+                heading: {
+                    type: String,
+                    trim: true,
+                },
+                content: {
+                    type: String,
+                },
+            },
+        ],
+        faq: [
+            {
+                question: {
+                    type: String,
+                    trim: true,
+                },
+                answer: {
+                    type: String,
+                },
+            },
+        ],
+        read_time_minutes: { 
+            type: Number,
+        },
 
-    createdAt: {
-      type: Date,
-      default: Date.now,
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
     },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 const Blog = mongoose.model("Blog", blogSchema);
